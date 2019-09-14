bffd1059ec051270b4512682ab3d405357c6a67a
jayabalandevops <balaniaf@gmail.com>
2019-09-14 12:45:46 +0530

    pipe line job

---
+++ pljob.groovy
@@ -0,0 +1,21 @@
+pipelineJob('pljob') {
+	description('First Pipeline job')
+	displayName('First Pipeline Job')   
+	node('master')
+	stage('Prepare'){
+		println('Prepare Stage...')
+	}
+	stage('Clone git'){
+		println('Cloning git...')
+	}
+	stage('Build'){
+		println('Building...')
+	}
+	stage('Deploy'){
+		println('Deploy...')
+	}
+	stage('Notify'){
+		println('Notifying...)
+	}
+}
+
