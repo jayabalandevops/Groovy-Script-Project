23badb4f045096ebf89b13932f37177a2df73452
jayabalandevops <balaniaf@gmail.com>
2019-09-14 12:01:50 +0530

    added ssh path to fsj

--- fsj.groovy
+++ fsj.groovy
@@ -2,7 +2,7 @@ freeStyleJob('fsj') {
     logRotator(-1, 10)
     jdk('localJDK')
     scm {
-        github('jayabalandevops/Groovy-Script-Project', 'master')
+        github('jayabalandevops/Groovy-Script-Project', 'master', 'ssh')
     }
     displayName('GroovyScriptFSJ')
     description('Groovy Script Free Style Project')
---
+++ fsjfolder.groovy
@@ -0,0 +1,10 @@
+freeStyleJob('fsj-folder') {
+    logRotator(-1, 10)
+    jdk('localJDK')
+    scm {
+        github('jayabalandevops/Groovy-Script-Project', 'master', 'ssh')
+    }
+    displayName('GroovyScriptFSJ-Folder')
+    description('Groovy Script Free Style Project creates Folder')
+}
+
