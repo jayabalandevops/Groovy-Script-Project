628833c73aa4ec7bdc12a0e2ccb79f92fe401b62
jayabalandevops <balaniaf@gmail.com>
2019-09-14 13:13:04 +0530

    again

---
+++ ci.groovy
@@ -0,0 +1,22 @@
+pipelineJob('pljob') {
+	description('First Pipeline job')
+	displayName('First Pipeline Job')   
+	node('master'){
+	stage('Prepare'){
+		println('Prepare Stage...')
+	}
+	stage('Clone git'){
+		println('Cloning git...')
+	}
+	stage('Build'){
+		println('Building...')
+	}
+	stage('Deploy'){
+		println('Deploy...')
+	}
+	stage('Notify'){
+		println('Notifying...')
+	}
+}
+}
+
--- pljob.groovy
+++ pljob.groovy
@@ -1,22 +1,5 @@
 pipelineJob('pljob') {
 	description('First Pipeline job')
 	displayName('First Pipeline Job')   
-	node('master'){
-	stage('Prepare'){
-		println('Prepare Stage...')
-	}
-	stage('Clone git'){
-		println('Cloning git...')
-	}
-	stage('Build'){
-		println('Building...')
-	}
-	stage('Deploy'){
-		println('Deploy...')
-	}
-	stage('Notify'){
-		println('Notifying...')
-	}
-}
 }
 
